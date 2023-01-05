01b719572d0c438998387a82078f5d58e4e7a5be
ShineYu <shine.yulingle@gmail.com>
2023-01-06 05:33:28 +0800

    Site updated: 2023-01-06 05:33:27

--- js/shine.js
+++ js/shine.js
@@ -34,7 +34,18 @@ var shine = {
             }
             ))
         }
+    },
+    getTimeState: function() {
+        var e = (new Date).getHours()
+          , t = "";
+        return 0 <= e && e <= 5 ? t = "晚安😴" : 5 < e && e <= 10 ? t = "早上好👋" : 10 < e && e <= 14 ? t = "中午好👋" : 14 < e && e <= 18 ? t = "下午好👋" : 18 < e && e <= 24 && (t = "晚上好👋"),
+        t
+    },
+    sayhi: function() {
+        var e = document.getElementById("author-info__sayhi");
+        e && (e.innerHTML = shine.getTimeState() + "！我是")
     }
 }
 
 shine.randomLink();
+shine.sayhi();

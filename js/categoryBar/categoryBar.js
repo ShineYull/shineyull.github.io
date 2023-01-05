4eda9bb932a3f4b86066a58d2a7cbc1e169e4437
ShineYu <shine.yulingle@gmail.com>
2023-01-05 16:17:13 +0800

    Site updated: 2023-01-05 16:17:10

---
+++ js/categoryBar/categoryBar.js
@@ -0,0 +1,45 @@
+categoriesBarActive()
+topCategoriesBarScroll()
+
+//分类条
+function categoriesBarActive(){
+  var urlinfo = window.location.pathname;
+  urlinfo = decodeURIComponent(urlinfo)
+  console.log(urlinfo);
+  //判断是否是首页
+  if (urlinfo == '/'){
+    if (document.querySelector('#category-bar')){
+      document.getElementById('主页').classList.add("select")
+    }
+  }else {
+    // 验证是否是分类链接
+    var pattern = /\/categories\/.*?\//;
+    var patbool = pattern.test(urlinfo);
+    console.log(patbool);
+    // 获取当前的分类
+    if (patbool) {
+      var valuegroup = urlinfo.split("/");
+      console.log(valuegroup[2]);
+      // 获取当前分类
+      var nowCategorie = valuegroup[2];
+      if (document.querySelector('#category-bar')){
+        document.getElementById(nowCategorie).classList.add("select");
+      }
+    }
+  }
+  
+}
+
+//鼠标控制横向滚动
+function topCategoriesBarScroll(){
+  if (document.getElementById("category-bar-items")){
+    let xscroll = document.getElementById("category-bar-items");
+  xscroll.addEventListener("mousewheel", function (e) {
+    //计算鼠标滚轮滚动的距离
+    let v = -e.wheelDelta / 2;
+    xscroll.scrollLeft += v;
+    //阻止浏览器默认方法
+    e.preventDefault();
+}, false);
+  }
+}
---
+++ js/shine.js
@@ -0,0 +1,40 @@
+"use strict";
+var shine = {
+    saveData: (e,t)=>{
+        localStorage.setItem(e, JSON.stringify({
+            time: Date.now(),
+            data: t
+        }))
+    },
+    loadData: (e,t)=>{
+        let n = JSON.parse(localStorage.getItem(e));
+        if (n) {
+            let e = Date.now() - n.time;
+            if (e < 60 * t * 1e3 && e > -1)
+                return n.data
+        }
+        return 0
+    },
+    randomLink: ()=>{
+        let e = shine.loadData("links", 30);
+        if (e) {
+            let t = document.querySelectorAll("#friend-links-in-footer .footer-item");
+            if (!t.length)
+                return;
+            for (let n = 0; n < 5; n++) {
+                let o = parseInt(Math.random() * e.length);
+                t[n].innerText = e[o].name,
+                t[n].href = e[o].link,
+                e.splice(o, 1)
+            }
+        } else{
+            fetch("/link.json").then((e=>e.json())).then((e=>{
+                shine.saveData("links", e.link_list),
+                shine.randomLink()
+            }
+            ))
+        }
+    }
+}
+
+shine.randomLink();

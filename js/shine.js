7d6b45f6d6852780d95401b38443ff0615478406
ShineYu <shine.yulingle@gmail.com>
2023-01-27 01:05:05 +0800

    Site updated: 2023-01-27 01:05:04

--- js/shine.js
+++ js/shine.js
@@ -126,6 +126,21 @@ var shine = {
     is_mobile: function() {
         return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
     },
+    //滚动到指定id
+    scrollTo:function(id){
+        var domTop = document.querySelector(id).offsetTop;
+        window.scrollTo(0,domTop - 80);
+    },
+    applyFlink: function() {
+        var input = document.getElementsByClassName('el-textarea__inner')[0];
+        let evt = document.createEvent('HTMLEvents');
+        evt.initEvent('input', true, true);
+        input.value = '昵称（请勿包含博客等字样）：\n网站地址（要求博客地址，请勿提交个人主页）：\n头像图片url（请提供尽可能清晰的图片，我会上传到我自己的图床）：\n描述：\n';
+        input.dispatchEvent(evt);
+        shine.scrollTo("#post-comment");
+        input.focus();
+        input.setSelectionRange(-1, -1);
+    },
 }
 
 shine.randomLink();
